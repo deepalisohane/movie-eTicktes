a6e1b72277e94f7cb09f7062a3351218cfda82bc
deepali.sohane <deepali.sohane@flick2know.com>
2024-03-07 17:55:13 +0530

    Add project files.

---
+++ eTicktes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTicktes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
